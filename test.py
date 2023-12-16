575b72bf47f19f2b8a66099ec5fe0691f6bdc15c
dai gang <384633887@qq.com>
2023-12-16 18:16:37 +0800

    remove minor issues

--- test.py
+++ test.py
@@ -90,8 +90,6 @@ def main(opt):
                         """intends to blur the boundaries of each sample to fit the actual using situations,
                             as suggested in 'Deep imitator: Handwriting calligraphy imitation via deep attention networks'"""
                         sk_pil = coords_render(preds[i], split=True, width=256, height=256, thickness=8, board=0)
-                        #sk_pil = sk_pil.resize((113, 113))
-
                         character = char_dict[character_id[i].item()]
                         save_path = os.path.join(opt.save_dir, 'test',
                                         str(writer_id[i].item()) + '_' + character+'.png')
